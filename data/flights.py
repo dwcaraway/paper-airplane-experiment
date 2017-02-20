d6d5d4b5cfb8dcf541779a73a6da84ef1419d6c0
Dave Caraway <dave@fogmine.com>
2017-02-19 22:36:04 -0500

    cleaned up the gh pages, added index.html

--- data/flights.py
+++
@@ -1,9 +0,0 @@
-"""
-Flight performance (distance for paper airplane experiment)
-"""
-
-import pandas
-
-from os.path import dirname, join
-
-flights = pandas.read_csv(join(dirname(__file__), 'flights.csv'))
